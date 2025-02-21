307b2a4505c69e3da4629e5a633c6605720c945c
Ivan Valadez <shadowlink46@gmail.com>
2025-02-21 04:51:35 -0500

    feat: music player!

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
-let click = () => {
-    alert("hiya!")
+let audio_play_button = document.getElementById("audio-play")
+let audio_prev_button = document.getElementById("audio-prev")
+let audio_skip_button = document.getElementById("audio-skip")
+
+let player
+function onYouTubeIframeAPIReady() {
+    console.log("player init")
+    player = new YT.Player('player', {
+        height: 1,
+        width: 1,
+        playerVars: {
+            playersInline: 1,
+            controls: 0,
+            loop: 1,
+            listType: "playlist",
+            list: "PL9N0sSHZNHhdPW1cAO7vVQCwN5yi9PBrV"
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    })
+    document.getElementById('player').style.visibility = 'hidden'
+}
+
+function onPlayerReady(event) {
+    event.target.setVolume(30)
+    event.target.playVideo()
+    console.log("player ready!")
+}
+
+let yt_play_button = () => {
+    let status = player.getPlayerState()
+    if (status === 1) {
+        player.pauseVideo()
+
+        let icon = document.getElementById("audio_button_id")
+        icon.textContent = "play_arrow"
+    } else if (status === -1 || status === 2) {
+        player.setVolume(30)
+        player.playVideo()
+
+        let icon = document.getElementById("audio_button_id")
+        icon.textContent = "pause"
+    }
+}
+
+audio_play_button.addEventListener('click', yt_play_button)
+audio_prev_button.addEventListener('click', () => player.previousVideo())
+audio_skip_button.addEventListener('click', () => player.nextVideo())
+
+function onPlayerStateChange(event) {
+    console.log("player state change")
+
+    let status = player.getPlayerState()
+    if (status === 1) {
+        let icon = document.getElementById("audio_button_id")
+        icon.textContent = "pause"
+    }
+}
+
+function stopVideo () {
+    player.stopVideo()
 }
